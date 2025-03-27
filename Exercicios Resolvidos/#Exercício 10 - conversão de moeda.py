277453126908a6cc2209af1b467905cd1fe92acf
igormitsuo <igormitsuo@gmail.com>
2025-03-27 04:28:10 -0300

    Update #Exercício 10 - conversão de moeda.py
    Teste de modificação

--- Exercicios Resolvidos/#Exercício 10 - conversão de moeda.py
+++ Exercicios Resolvidos/#Exercício 10 - conversão de moeda.py
@@ -2,3 +2,5 @@ real=float(input("digite quanto dinheiro voce tem na carteira R$"))
 dolar= real/5.25
 print("com R$ {}, voce pode comprar US$ {:.2f} ".format(real, dolar))
 
+#teste de alteração de arquivo para versonamento no github#
+#teste 02
